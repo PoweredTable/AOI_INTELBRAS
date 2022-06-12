1832a83508d03a2395440d6a1a33ed94eb6c17d2
lucas GG <poweredlucas@gmail.com>
2022-06-12 19:21:31 -0300

    Mensagens de console movidas para onde devem estar

--- console_log.py
+++ console_log.py
@@ -1,11 +1,35 @@
-creating_arduino_file_warn = '<br><pre style="text-align: left;"><span style="font-family: Arial, Helvetica, ' \
-                             'sans-serif; ' \
-                             'font-size: 19px; ''background-color: rgb(250, 197, 28);">>>> warning:  Arquivo de ' \
-                             'configura&ccedil;&otilde;es padr&atilde;o do ''Arduino foi criado, recomenda-se ' \
-                             'ajuste.</span></pre> '
+creating_arduino_file_warn = '<p><span style="font-family: Arial, Helvetica, sans-serif; font-size: 19px; color: rgb(' \
+                             '251, 160, 38);">🔶 Aviso: </span><span style="font-family: Arial, Helvetica, ' \
+                             'sans-serif; font-size: 19px;">Arquivo de configura&ccedil;&otilde;es padr&atilde;o do ' \
+                             'Arduino foi criado, recomenda-se ajuste.</span></p><br> '
 
-arduino_not_found_error = '<br><p><span style="font-family: Arial, Helvetica, sans-serif; font-size: 19px; ' \
-                          'background-color: rgb(138, 124, 123);">&gt;&gt;&gt;critical:  Arduino n&atilde;o ' \
-                          'encontrado, ''instale o driver <a href="https://drive.google.com/drive/folders' \
-                          '/1fCUTciTNCfYBJtet1WClLt9V' \
-                          '-o6wJ-wu?usp=sharing">CH340</a> e verifique a conex&atilde;o com o Arduino.</span></p>'
+arduino_not_found_error = '<p><span style="font-family: Arial, Helvetica, sans-serif; font-size: 18px;">🔺 <span ' \
+                          'style="color: rgb(184, 49, 47);">Erro de conex&atilde;o:</span> Arduino n&atilde;o ' \
+                          'encontrado, verifique se o <a ' \
+                          'href="https://drive.google.com/drive/folders/1fCUTciTNCfYBJtet1WClLt9V-o6wJ-wu?usp=sharing' \
+                          '" rel="noopener noreferrer" target="_blank">driver de convers&atilde;o</a> est&aacute; ' \
+                          'instalado e tente novamente.</span></p><br> '
+
+main_window_title = '<pre style="text-align: center;"><span style="font-family: Arial, '\
+                                            'Helvetica, sans-serif; font-size: 22px;">Inspe&ccedil;&atilde;o '\
+                                            '&oacute;ptica autom&aacute;tica: {} - Interface de '\
+                                            'controle</span></pre><br>'
+
+manually_starting_inspection = '<hr><p style="text-align: center;"><span style="font-family: ' \
+                               'Arial, Helvetica, sans-serif; font-size: 20px;">🔧<span ' \
+                               'style="color: rgb(65, 168, 95);">&nbsp;</span><span ' \
+                               'style="color: rgb(124, 112, 107);">Controle manual: ' \
+                               '</span>Inspe&ccedil;&atilde;o ' \
+                               'inicializando...</span></p><hr><p></p>'
+
+manually_stopping_inspection = '<hr><p style="text-align: center;"><span style="font-family: '\
+                                                    'Arial, Helvetica, sans-serif; font-size: 20px;">🔧<span '\
+                                                    'style="color: rgb(65, 168, 95);">&nbsp;</span><span '\
+                                                    'style="color: rgb(124, 112, 107);">Controle '\
+                                                    'manual:&nbsp;</span>Inspe&ccedil;&atilde;o '\
+                                                    'desligada!</span></p><hr><p><br></p>'
+
+no_digital_inputs_active = '<p><span style="font-family: Arial, Helvetica, sans-serif; '\
+                                                   'font-size: 19px;"><span style="color: rgb(44, 130, '\
+                                                   '201);">🔷 Informa&ccedil;&atilde;o:</span> Nenhuma entrada '\
+                                                   'digital foi configurada.</span></p><br>'
