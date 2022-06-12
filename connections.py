a9d5a0459d520a8972c99d368ac46ced3529dd8e
lucas GG <poweredlucas@gmail.com>
2022-06-12 00:44:56 -0300

    a few changes to the connections.py file

--- connections.py
+++ connections.py
@@ -7,6 +7,12 @@ class ArduinoCon:
         pass
 
 
+def clean_up_pins(signal='analog'):
+    if signal == 'digital':
+        pass
+    return {0: False, 1: False, 2: False, 3: False, 4: False, 5: False, 6: False, 7: False}
+
+
 class CamerasCon:
     def __init__(self, parameters):
         pass
