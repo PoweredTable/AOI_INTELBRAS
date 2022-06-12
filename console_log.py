0625bb43a9b426154fce3040fb736f7f61151a77
lucas GG <poweredlucas@gmail.com>
2022-06-12 00:46:00 -0300

    arquivo de mensagens de log para melhor visualização

---
+++ console_log.py
@@ -0,0 +1,11 @@
+creating_arduino_file_warn = '<br><pre style="text-align: left;"><span style="font-family: Arial, Helvetica, ' \
+                             'sans-serif; ' \
+                             'font-size: 19px; ''background-color: rgb(250, 197, 28);">>>> warning:  Arquivo de ' \
+                             'configura&ccedil;&otilde;es padr&atilde;o do ''Arduino foi criado, recomenda-se ' \
+                             'ajuste.</span></pre> '
+
+arduino_not_found_error = '<br><p><span style="font-family: Arial, Helvetica, sans-serif; font-size: 19px; ' \
+                          'background-color: rgb(138, 124, 123);">&gt;&gt;&gt;critical:  Arduino n&atilde;o ' \
+                          'encontrado, ''instale o driver <a href="https://drive.google.com/drive/folders' \
+                          '/1fCUTciTNCfYBJtet1WClLt9V' \
+                          '-o6wJ-wu?usp=sharing">CH340</a> e verifique a conex&atilde;o com o Arduino.</span></p>'
